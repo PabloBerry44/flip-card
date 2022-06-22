23e353fb068aab05d3f45174349398f559e51efb
pabloberry44 <pawel.borowiec@tutanota.com>
2022-06-22 21:57:56 +0200

    javascript generates a number of cards according to difficulty, and to every card adds event listener to add or remove class 'flip'

--- script.js
+++ script.js
@@ -1,13 +1,59 @@
-const innerFlip = document.querySelector('.cardWrap-inner')
-let flipped = false
+const diffButton = document.querySelectorAll('.difficulty')
+const gameStartView = document.querySelector('.startGame')
+const playground = document.querySelector('.playground')
 
-innerFlip.addEventListener('click', ()=>{
-    if(flipped){
-        innerFlip.classList.remove('flip')
-        flipped = false
+for (const button of diffButton) {
+    button.addEventListener('click', () => startGame(button.textContent))
+}
+
+function startGame(difficulty){
+    let numOfCards
+    let gapsSize
+    let inRow
+    switch(difficulty){
+        case 'Easy': numOfCards=8, gapsSize=40, inRow=3; break;
+        case 'Medium': numOfCards=16, gapsSize=50, inRow=4; break;
+        case 'Hard': numOfCards=36, gapsSize=60, inRow=6; break;
     }
-    else{
-        innerFlip.classList.add('flip')
-        flipped = true
+
+    gameStartView.style.opacity = 0
+    setTimeout(()=>{gameStartView.style.display = 'none'}, 100)
+
+    for(q=0; q<numOfCards; q++){
+        
+        cardWrap = document.createElement('div')
+        cardWrap.classList.add('cardWrap')
+        cardWrap.style.width = ((650-gapsSize)/inRow) + 'px'
+        cardWrap.style.height = ((650-gapsSize)/inRow) + 'px'
+
+        const cardWrapInner = document.createElement('div')
+        cardWrapInner.classList.add('cardWrap-inner')
+
+        const cardWrapFront = document.createElement('div')
+        cardWrapFront.innerHTML = "?"
+        cardWrapFront.classList.add('cardWrap-front')
+
+        const cardWrapBack = document.createElement('div')
+        cardWrapBack.classList.add('cardWrap-back')
+
+        playground.appendChild(cardWrap)
+        cardWrap.appendChild(cardWrapInner)
+        cardWrapInner.appendChild(cardWrapFront)
+        cardWrapInner.appendChild(cardWrapBack)
+
+        if(numOfCards==8 && q==4){
+            console.log('es')
+            cardWrap.style.transform = 'translateY(211px)'
+        }
+
+        cardWrap.addEventListener('click', ()=>{
+            if(cardWrapInner.classList.contains('flip')){
+                cardWrapInner.classList.remove('flip')
+            }
+            else{
+                cardWrapInner.classList.add('flip')
+            }
+        })
     }
-})
+
+}
